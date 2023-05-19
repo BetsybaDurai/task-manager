bbd4ad896803ae3e7ada6ed5454c86b72383a363
gnana.betsyba@goavega.com <gnana.betsyba@goavega.com>
2023-05-19 10:35:16 +0530

    Resource reading endpoints

--- src/index.js
+++ src/index.js
@@ -15,11 +15,53 @@ app.post('/users', (req, res) => {
     }).catch((e) => {
         // res.status(400) // setting the status for response
         res.status(400).send(e)
-        res.send(e)
+       
+    })
+})
+
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => { //fetching all users
+        res.send(users)
+    }).catch((e) => {
+        res.status(500).send()
+     })
+})
+
+app.get('/users/:id', (req, res) => {
+    // console.log(req.params)
+    const _id = req.params.id; //didn't change to object or string
+    User.findById(_id).then((user) => {
+        if (!user) {
+            res.status(404).send(user)
+        }
+        res.send(user)
+    }).catch((e)=> {
+        res.status(500).send(e)
     })
 })
 
 
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => { //fetching all tasks
+        res.send(tasks)
+    }).catch((e) => {
+        res.status(500).send()
+     })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    // console.log(req.params)
+    const _id = req.params.id; //didn't change to object or string
+    Task.findById(_id).then((task) => {
+        if (!task) {
+            res.status(404).send(task)
+        }
+        res.send(task)
+    }).catch((e)=> {
+        res.status(500).send(e)
+    })
+})
 app.post('/tasks', (req, res) => {
     // console.log(req.body)
     // res.send('testing!')
@@ -29,7 +71,7 @@ app.post('/tasks', (req, res) => {
     }).catch((e) => {
         // res.status(400) // setting the status for response
         res.status(201).status(400).send(e)
-        res.send(e)
+        // res.send(e)
     })
 })
 app.listen(port, () => {
